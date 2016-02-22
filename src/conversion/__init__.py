7a6c243a18c95402fcc8b97c02ed8b0013f29be5
Onno Valkering <onno.valkering@gmail.com>
2016-02-22 19:57:07 +0100

    Added zip pipeline link placeholder

---
+++ src/conversion/__init__.py
@@ -0,0 +1,1 @@
+from conversion.zip import zipcompression
---
+++ src/conversion/zip/zipcompression.py
@@ -0,0 +1,9 @@
+
+class ZipCompression:
+    """A pipeline link for zip compression"""
+
+    def compress(self):
+        pass
+
+    def decompress(self):
+        pass
