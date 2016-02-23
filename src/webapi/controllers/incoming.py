8168c173729b75e63c1bd4e405ef6abfd1d65469
Onno Valkering <onno.valkering@gmail.com>
2016-02-23 14:32:39 +0100

    Quick debug statement

--- src/webapi/controllers/incoming.py
+++ src/webapi/controllers/incoming.py
@@ -1,7 +1,11 @@
+from flask import request
 from webapi import app
 from webapi.helpers.responses import *
 
 
 @app.route('/incoming', methods=['POST'])
 def incoming():
-    return notimplemented()
+    body = request.get_json()
+    print(body)
+
+    return nocontent()
