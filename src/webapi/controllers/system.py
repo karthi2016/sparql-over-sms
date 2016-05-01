b737c042d8b1dddae2c1d0fdeda57a2ba69e3dfd
Onno Valkering <onno.valkering@gmail.com>
2016-05-01 21:57:07 +0200

    Eleborated getting started

--- src/webapi/controllers/system.py
+++ src/webapi/controllers/system.py
@@ -6,19 +6,10 @@ from webapi.helpers.responses import *
 
 @app.route('/')
 def get_status():
-    with open('./../releaseversion.txt', 'r') as f:
-        releaseversion = f.readline()
-
-    return ok({'name': 'SPARQL over SMS', 'version': releaseversion})
+    return ok({'name': 'SPARQL over SMS', 'version': app.releaseversion})
 
 
 @inject(messagerepo=repositories.MessageRepo)
 @app.route('/messages')
 def messages(messagerepo):
     return ok(messagerepo.get_messages())
-
-
-
-
-
-
