9953076266acea58fd5b2b56777d52dfdee7e72a
Onno Valkering <onno.valkering@gmail.com>
2016-06-19 11:59:07 +0200

    Added incoming endpoint (tornado)

---
+++ sos-service/src/webapi/endpoints/incoming.py
@@ -0,0 +1,22 @@
+from tornroutes import route
+from transfer.models import IncomingMessage
+from webapi.handlers import HttpHandler
+from webapi.helpers import badrequest
+
+
+@route('/incoming')
+class SparqlUpdate(HttpHandler):
+
+    def post(self):
+        sender = self.get_parameter('sender')
+        content = self.get_parameter('content')
+
+        if sender is None:
+            raise badrequest('parameter "sender" not provided')
+
+        if content is None:
+            raise badrequest('parameter "content" not provided')
+
+        message = IncomingMessage(3, content, sender)
+        self.set_status(202)
+
