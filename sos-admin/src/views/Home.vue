0959bfceb55b84c2c9e9654d4073ffdea534163c
onnovalkering <onno.valkering@gmail.com>
2017-01-05 13:49:06 +0100

    More placeholders

--- sos-admin/src/views/Home.vue
+++ sos-admin/src/views/Home.vue
@@ -1,16 +1,78 @@
 <template>
-    <div id="home" class="row">
-        <div class="col-md-12">
-            <b-alert show="true" state="info">
-                The SPARQL over SMS administration client is still under development.
-            </b-alert>
+    <div id="home">
+        <div class="row">
+            <div class="col-md-12">
+                <div class="alert alert-info" role="alert">
+                    <strong>Heads up!</strong> The SPARQL over SMS administration client is still under development...
+                </div>          
+            </div>
+        </div>
+        <div class="row">
+            <div class="col-md-6">
+                <div class="card">
+                    <div class="card-block">
+                        <h4 class="card-title">Incoming</h4>
+                        <h6 class="card-subtitle text-muted">Over the last 3 days.</h6>
+                    </div>
+                    <div class="card-block">
+                        <p class="card-text">...</p>
+                    </div>                    
+                </div>
+            </div>
+            <div class="col-md-6">
+                <div class="card">
+                    <div class="card-block">
+                        <h4 class="card-title">Outgoing</h4>
+                        <h6 class="card-subtitle text-muted">Over the last 3 days.</h6>
+                    </div>
+                    <div class="card-block">
+                        <p class="card-text">...</p>
+                    </div>
+                </div>
+            </div>
+        </div>
+        <div class="row">
+            <div class="col-md-4">
+                <div class="card">
+                    <div class="card-block">
+                        <h4 class="card-title">Shared Knowledge</h4>
+                        <h6 class="card-subtitle text-muted">Currently.</h6>
+                    </div>
+                    <div class="card-block">
+                        <p class="card-text">...</p>
+                    </div>
+                </div>                
+            </div>
+            <div class="col-md-4">
+                <div class="card">
+                    <div class="card-block">
+                        <h4 class="card-title">Compression</h4>
+                        <h6 class="card-subtitle text-muted">Over the last 3 days.</h6>
+                    </div>
+                    <div class="card-block">
+                        <p class="card-text">...</p>
+                    </div>
+                </div>                
+            </div>
+            <div class="col-md-4">
+                <div class="card">
+                    <div class="card-block">
+                        <h4 class="card-title">Decompression</h4>
+                        <h6 class="card-subtitle text-muted">Over the last 3 days.</h6>
+                    </div>
+                    <div class="card-block">
+                        <p class="card-text">...</p>
+                    </div>
+                </div>                
+            </div>                        
         </div>
     </div>
+
 </template>
 
 <script>
 export default {
-  name: 'contacts',
+  name: 'home',
 };
 </script>
 
