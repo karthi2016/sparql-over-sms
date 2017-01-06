864469cb6ea21d4277b555b9ec61cb461dc2d621
onnovalkering <onno.valkering@gmail.com>
2017-01-06 21:02:50 +0100

    Elaborated messages view

--- sos-admin/src/views/Messages.vue
+++ sos-admin/src/views/Messages.vue
@@ -1,8 +1,36 @@
 <template>
     <div id="messages">
         <div class="row">
+          <div class="col-md-12">
+            <button v-on:click="refreshMessages" class="btn btn-outline-secondary">Refresh</button>
+            <br /><br />
+          </div>
+        </div>
+        <div class="row">
             <div class="col-md-12">
-                Messages will be managable from this page.
+              <table class="table">
+                <thead class="thead-default">
+                  <tr>
+                    <th width="20%">Correlation Id</th>
+                    <th width="20%">Sender</th>
+                    <th width="20%">Reciever</th>
+                    <th width="20%">Category</th>
+                    <th width="20%"></th>
+                  </tr>
+                </thead>
+                <tbody>
+                  <tr v-for="message in messages">
+                    <td>{{message.id}}</td>
+                    <td>{{message.sender}}</td>
+                    <td>{{message.reciever}}</td>
+                    <td>{{message.category}}</td>
+                    <td>
+                      <button v-on:click="viewMessage(message)" class="btn btn-sm btn-outline-info">Details</button>
+                      <button v-on:click="deleteMessage(message)" class="btn btn-sm btn-outline-warning">Delete</button>
+                    </td>
+                  </tr>
+                </tbody>
+              </table>
             </div>
         </div>
     </div>
@@ -11,6 +39,36 @@
 <script>
 export default {
   name: 'messages',
+
+  data() {
+    return {
+      messages: [],
+    };
+  },
+
+  mounted() {
+    this.refreshMessages();
+  },
+
+  methods: {
+    refreshMessages() {
+      this.$http.get('http://localhost:8888/messages').then((response) => {
+        this.messages = response.body;
+      });
+    },
+
+    viewMessage(message) {
+      console.log(message);
+    },
+
+    deleteMessage(message) {
+      const messageId = message.id;
+
+      this.$http.delete(`http://localhost:8888/message/${messageId}`).then(() => {
+        this.refreshMessages();
+      });
+    },
+  },
 };
 </script>
 
